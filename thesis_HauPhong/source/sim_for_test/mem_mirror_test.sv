2b026de45d5dcba578265d6819180eaf7c953a94
hant225 <146408258+hant225@users.noreply.github.com>
2024-03-23 23:57:40 +0700

    Add files via upload

--- thesis_HauPhong/source/sim_for_test/mem_mirror_test.sv
+++ thesis_HauPhong/source/sim_for_test/mem_mirror_test.sv
@@ -1,12 +1,53 @@
 `timescale 1ns / 1ps
 
 typedef logic [63:0] ultra_ram [];
+typedef logic [63:0] ultra_ram_queue [$];
+typedef enum bit[3:0] { RESET,  
+                        MEM_KERNEL_LOAD,
+                        MEM_BIAS_LOAD,
+                        MEM_SCALE_LOAD,
+                        DATA_IN_LOAD} oper_mode;
 
 module mem_mirror_test;
-    ultra_ram kernel_mem [int];    
+    ultra_ram kernel_mem [logic]; 
+    ////////////////////////////////
+    ultra_ram_queue virtual_mem [int];
+    oper_mode op = MEM_KERNEL_LOAD; 
+    logic [31:0] weight_addr = 32'h4000_0000;
+    logic [63:0] weight_data;
+    
+    int a;
     
     initial begin
-        // initialize
+        for(int i = weight_addr; i < weight_addr + 25; i++) begin
+            if(i < weight_addr + 9) begin
+                repeat(4) begin
+                    weight_data = $random();
+                    virtual_mem[i].push_back(weight_data);
+                end
+            end else begin
+                weight_data = $random();
+                virtual_mem[i].push_back(weight_data);
+            end
+        end
+        
+        
+        foreach(virtual_mem[i]) begin
+            $write("key: %0h ,", i);
+            foreach(virtual_mem[i][j]) begin
+                $write("  { pos : %0d  data = %16h }  ", j, virtual_mem[i][j]);
+            end
+            $write("\n");    
+        end 
+        $display("%p", virtual_mem);
+        
+    end
+    
+    
+    
+    // Randomize mem with correct structure
+    function randomize_mem;
+         // initialize
         for(int i = 0;  i < 30; i++) begin
             if(i < 9) begin
                 kernel_mem[i] = new[4];
@@ -14,10 +55,6 @@ module mem_mirror_test;
                 kernel_mem[i] = new[1];
             end
         end
-        
-        // check size
-        //  foreach(kernel_mem[i])
-        //      $display("key = %0d , size = %0d", i, kernel_mem[i].size());
         
         // give random value
         foreach(kernel_mem[i]) begin
@@ -36,5 +73,5 @@ module mem_mirror_test;
             end
             $write("\n");
         end
-    end
+    endfunction
 endmodule
