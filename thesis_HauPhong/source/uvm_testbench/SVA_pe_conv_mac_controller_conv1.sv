a3871e76f0d29efec11fa1d2d85c47eef023d72a
hant225 <146408258+hant225@users.noreply.github.com>
2024-06-23 15:27:09 +0700

    Add files via upload

--- thesis_HauPhong/source/uvm_testbench/SVA_pe_conv_mac_controller_conv1.sv
+++ thesis_HauPhong/source/uvm_testbench/SVA_pe_conv_mac_controller_conv1.sv
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
-
+`include "uvm_macros.svh"
+import uvm_pkg::*;
 
 module SVA_pe_conv_mac_controller_conv1 #(   
      parameter  pOUT_CHANNEL      = 32    
@@ -32,20 +33,27 @@ module SVA_pe_conv_mac_controller_conv1 #(
     sequence s_buffer_valid;
         @(posedge clk) $rose(buffer_valid) ##1 $fell(buffer_valid);
     endsequence      
+   
     
-    A_valid:      assert property (@(posedge clk) $rose(buffer_valid) |-> $rose(valid) ##1 ($fell(valid) and $fell(buffer_valid)))
-                    $info("@%0t A_valid success", $time);
     A_adder_en:   assert property (@(posedge clk) s_buffer_valid |-> ##4 $rose(adder_en) ##1 $fell(adder_en))
-                    $info("@%0t A_adder_en success", $time);                      
-    A_bias_en:    assert property (@(posedge clk) s_buffer_valid |-> ##3 $rose(bias_en) ##1 $fell(bias_en))
-                    $info("@%0t A_bias_en success", $time);          
-    A_act_en:     assert property (@(posedge clk) s_buffer_valid |-> ##4 $rose(act_en) ##1 $fell(act_en))
-                    $info("@%0t A_act_en success", $time);                                      
+                    `uvm_info("SVA", $sformatf("@%0t A_adder_en success", $time), UVM_NONE);
+                         
     A_dequant_en: assert property (@(posedge clk) s_buffer_valid |-> ##5 $rose(dequant_en) ##1 $fell(dequant_en))
-                    $info("@%0t A_dequant_en success", $time);    
-    A_quant_en:   assert property (@(posedge clk) s_buffer_valid |-> ##8 $rose(quant_en) ##1 $fell(quant_en))
-                    $info("@%0t A_quant_en success", $time); 
-    A_buffer_en:  assert property (@(posedge clk) s_buffer_valid |-> ##9 $rose(buffer_en) ##1 $fell(buffer_en))
-                    $info("@%0t A_buffer_en success", $time);       
-                                                                           
+                    `uvm_info("SVA", $sformatf("@%0t A_dequant_en success", $time), UVM_NONE);
+                    
+    A_bias_en:    assert property (@(posedge clk) s_buffer_valid |-> ##14 $rose(bias_en) ##1 $fell(bias_en))
+                    `uvm_info("SVA", $sformatf("@%0t A_bias_en success", $time), UVM_NONE);
+                    
+    A_act_en:     assert property (@(posedge clk) s_buffer_valid |-> ##15 $rose(act_en) ##1 $fell(act_en))
+                    `uvm_info("SVA", $sformatf("@%0t A_act_en success", $time), UVM_NONE);
+                                 
+    A_quant_en:   assert property (@(posedge clk) s_buffer_valid |-> ##19 $rose(quant_en) ##1 $fell(quant_en))
+                    `uvm_info("SVA", $sformatf("@%0t A_quant_en success", $time), UVM_NONE);
+                    
+    A_buffer_en:  assert property (@(posedge clk) s_buffer_valid |-> ##20 $rose(buffer_en) ##1 $fell(buffer_en))
+                    `uvm_info("SVA", $sformatf("@%0t A_buffer_en success", $time), UVM_NONE);
+                            
+    A_valid:      assert property (@(posedge clk) s_buffer_valid |-> ##21 $rose(valid) ##1 $fell(valid))
+                    `uvm_info("SVA", $sformatf("@%0t A_valid success", $time), UVM_NONE);
+                                                       
 endmodule
