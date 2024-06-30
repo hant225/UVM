89cb4b4004f8af002e2d2b4fc0402c9dceba6d06
hant225 <146408258+hant225@users.noreply.github.com>
2024-06-30 19:09:03 +0700

    Add files via upload

---
+++ ShuffleNet_VIP/Script/run_dpi_lib_create.tcl
@@ -0,0 +1,31 @@
+# *** READEME ***
+# cd to C_file folder before running this script
+# set c_file_name <name of C file to compile>
+
+# Unset the default python3 (Environment Variables) in vivado lib to use the OS python
+unset -nocomplain ::env(PYTHONHOME)
+unset -nocomplain ::env(PYTHONPATH)
+
+# compile and link library (2-steps)
+exec xsc ../C_files/$c_file_name
+
+# get current active simset
+set active_sim_set [current_fileset -simset]
+
+# copy shared lib from compile dir to current simset xsim.dir
+## 1. get the destination path
+set dest_dir [get_property directory [current_project]]/[current_project].sim/$active_sim_set/behav/xsim/ 
+
+## 2. execute copy
+exec cp ./xsim.dir/work/xsc/dpi.so $dest_dir
+
+# xelab config
+set_property -name {xsim.elaborate.xelab.more_options} -value {-sv_lib "dpi.so" -L uvm} -objects [get_filesets $active_sim_set]
+set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_conv1]
+
+
+
+
+
+
+
