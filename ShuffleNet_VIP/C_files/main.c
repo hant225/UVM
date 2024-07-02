ebb69457b62a7bedda3d00428927b0fc889bc289
hant225 <146408258+hant225@users.noreply.github.com>
2024-07-02 20:31:06 +0700

    Add files via upload

--- ShuffleNet_VIP/C_files/main.c
+++ ShuffleNet_VIP/C_files/main.c
@@ -8,3 +8,6 @@ void gen_conv1_input_and_rslt() {
     system("/usr/bin/python3 /home/hao/Documents/0.KHOA_LUAN_TOT_NGHIEP/shuffle_net/cnn_my_verification/Python_files/conv1_randIimg_and_refModel.py");
 }
 
+void golden_model() {
+    system("/usr/bin/python3 /home/hao/Documents/0.KHOA_LUAN_TOT_NGHIEP/shuffle_net/cnn_my_verification/Python_files/compare_to_pytorch_rslt.py");
+}
